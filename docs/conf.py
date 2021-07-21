81bd80e4cff99a5c0aa36899168f6e9e5c41648d
Scott Human <scott.human@outlook.com>
2021-07-21 20:35:40 +1000

    furo

--- docs/conf.py
+++ docs/conf.py
@@ -33,10 +33,10 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+#extensions = []
 
 
-#extensions = [
+extensions = [
     'furo'
     #'sphinx_rtd_theme',
 ]
