325d485a5be945f407fb2c364f4cb3866cc9afa5
Scott Human <scott.human@outlook.com>
2021-07-21 20:31:33 +1000

    furop

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,6 @@
 #import guzzle_sphinx_theme
 #import sphinx_rtd_theme
 
-import furo
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
