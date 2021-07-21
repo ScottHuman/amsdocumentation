315398d6d3a48604a50e54eda046273c2c73a06a
Scott Human <scott.human@outlook.com>
2021-07-21 19:50:39 +1000

    sphinxrtd

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,4 @@
-import guzzle_sphinx_theme
+#import guzzle_sphinx_theme
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -32,7 +32,7 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = []
-extensions.append("guzzle_sphinx_theme")
+#extensions.append("guzzle_sphinx_theme")
 
 # Register the theme as an extension to generate a sitemap.xml
 
@@ -51,17 +51,17 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-##html_theme = 'sphinx_rtd_theme'
-html_theme = 'guzzle_sphinx_theme'
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'guzzle_sphinx_theme'
 
 # Guzzle theme options (see theme.conf for more information)
 html_theme_options = {
     # Set the name of the project to appear in the sidebar
-    "project_nav_name": "AMS Documentation",
+    #"project_nav_name": "AMS Documentation",
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
-html_theme_path = guzzle_sphinx_theme.html_theme_path()
+html_static_path = ['_static']
+#html_theme_path = guzzle_sphinx_theme.html_theme_path()
